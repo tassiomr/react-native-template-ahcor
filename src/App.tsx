d0fbf1a4b7bdee53d600a0e67f92443106b43b11
Tassio Rocha <tassiomarcosr@outlook.com>
2022-02-03 17:39:41 -0300

    Clear on app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import * as React from 'react';
+import { Home } from './pages';
+ 
+const App = () => <Home />;
 
- import * as React from 'react';
- import { Home } from './pages';
- 
- const App = () => {
-   return (
-     <Home />
-   );
- };
- 
-
- 
- export default App;
- 
+export default App;

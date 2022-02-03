1cbcb7b26741b68fa7135820539a1e04484eebfb
Tassio Rocha <tassiomarcosr@outlook.com>
2022-02-03 17:29:09 -0300

    Creating base template

---
+++ src/components/body/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  width: 100%;
+  alignItems: center;
+  paddingLeft: 8;
+  paddingRight: 8;
+  backgroundColor: white;
+`;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text } from 'react-native';
+import { Body } from '../components';
+
+export const Home: React.FC = () => {
+  return (<Body>
+           <Text>Tassio</Text>
+         </Body>)
+}
+

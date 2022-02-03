3312d10e141ab919adf8fccf8cad0429f5acbdc7
Tassio Rocha <tassiomarcosr@outlook.com>
2022-02-03 17:55:19 -0300

    Update app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import * as React from 'react';
+import { useColorScheme } from 'react-native'
+import { ThemeProvider } from 'styled-components';
+import Themes from './configs/theme';
 import { Home } from './pages';
  
-const App = () => <Home />;
+const App = () => {
+  const theme = useColorScheme();
+
+  return (
+    <ThemeProvider theme={Themes[theme || 'light']}>
+      <Home />;
+    </ThemeProvider>
+  );
+}
 
 export default App;

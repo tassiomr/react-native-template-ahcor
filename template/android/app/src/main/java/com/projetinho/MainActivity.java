f750982aab9f74a03bc45de89c2a145d9d533906
Tassio Rocha <tassiomarcosr@outlook.com>
2022-02-03 21:19:59 -0300

    Remane template

--- template/android/app/src/main/java/com/projetinho/MainActivity.java
+++ template/android/app/src/main/java/com/projetinho/MainActivity.java
@@ -1,4 +1,4 @@
-package com.projetinho;
+package com.AhcorTemplate;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "projetinho";
+    return "AhcorTemplate";
   }
 }
